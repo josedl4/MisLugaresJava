71355beca73cf0e7ee456c209023a123cfbb30d7
Jose Luis Martín <joseop21@gmail.com>
2016-09-03 02:08:03 +0200

    Completado el ejercicio del tema 1 de 'El Gran Libro de Android'.

--- mislugares/src/main/java/com/example/Lugares.java
+++ mislugares/src/main/java/com/example/Lugares.java
@@ -38,28 +38,29 @@ public class Lugares {
     public static ArrayList<Lugar> ejemploLugares(){
         ArrayList<Lugar> lugares = new ArrayList<Lugar>();
         lugares.add(new Lugar("Escuela Politecnica Superior de Gandia",
-                "C/ Paranimf, 1 46730 Gandia (SPAIN)", -0.166093, 38995656, 962849300,
+                "C/ Paranimf, 1 46730 Gandia (SPAIN)", -0.166093, 38.995656, 962849300,
                 "http://www.epsg.upv.es", "Uno de los mejores lugares para formarse",
                 5, TipoLugar.EDUCACION));
 
-        lugares.add(new Lugar("Escuela Politecnica Superior de Gandia",
-                "C/ Paranimf, 1 46730 Gandia (SPAIN)", -0.166093, 38995656, 962849300,
-                "http://www.epsg.upv.es", "Uno de los mejores lugares para formarse",
+        lugares.add(new Lugar("Al de siempre.",
+                "P.Industrial Junto Molí Nou - 46722, Benifla (Valencia)", -0.190642, 38.925857,
+                636472405, "", "No te pierdas el arroz en calabaza.",
+                3, TipoLugar.BAR));
+
+        lugares.add(new Lugar("androidcurso.com",
+                "ciberespacio", 0, 0, 962849300,
+                "http://androidcurso.com", "Amplia tus conocimientos sobre Android",
                 5, TipoLugar.EDUCACION));
 
-        lugares.add(new Lugar("Escuela Politecnica Superior de Gandia",
-                "C/ Paranimf, 1 46730 Gandia (SPAIN)", -0.166093, 38995656, 962849300,
-                "http://www.epsg.upv.es", "Uno de los mejores lugares para formarse",
-                5, TipoLugar.EDUCACION));
+        lugares.add(new Lugar("Barranco del Infierno.",
+                "Vía Verde del río Serpis. Villalonga (Valencia)", -0.295058, 38.867180, 0,
+                "http://sosegaos.blogspot.com.es", "Espectacular ruta para bici o andar.",
+                4, TipoLugar.NATURALEZA));
 
-        lugares.add(new Lugar("Escuela Politecnica Superior de Gandia",
-                "C/ Paranimf, 1 46730 Gandia (SPAIN)", -0.166093, 38995656, 962849300,
-                "http://www.epsg.upv.es", "Uno de los mejores lugares para formarse",
-                5, TipoLugar.EDUCACION));
-
-        lugares.add(new Lugar("Escuela Politecnica Superior de Gandia",
-                "C/ Paranimf, 1 46730 Gandia (SPAIN)", -0.166093, 38995656, 962849300,
-                "http://www.epsg.upv.es", "Uno de los mejores lugares para formarse",
-                5, TipoLugar.EDUCACION));
+        lugares.add(new Lugar("La Vital.",
+                "Avda. de La Vital, 0 46701 Gandia (Valencia)", -0.1720092, 38.9705949, 962881070,
+                "http://www.lavital.es/", "El tipico centro comercial",
+                5, TipoLugar.COMPRAS));
+        return lugares;
     }
 }
--- mislugares/src/main/java/com/example/Principal.java
+++ mislugares/src/main/java/com/example/Principal.java
@@ -5,8 +5,8 @@ package com.example;
  */
 public class Principal {
     public static void main(String[] main){
-        Lugar lugar = new Lugar("ETSII", "C/...", -0.166093, 38.995656, 962849300, "http://...",
-                "Escuela tecnica...", 10, TipoLugar.EDUCACION);
-        System.out.println(lugar.toString());
+        for (Lugar l: Lugares.vectorLugares){
+            System.out.println(l);
+        }
     }
 }
